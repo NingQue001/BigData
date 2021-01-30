9f02a2cc134548c87a6f010ff581ba04fbaefa5f
NingQue001 <296480474@qq.com>
2021-01-30 20:55:50 +0800

    伪异步IO

--- src/main/java/com/anven/BigData/zeroCopyAndNio/BIOServer.java
+++ src/main/java/com/anven/BigData/zeroCopyAndNio/BIOServer.java
@@ -5,10 +5,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * BIO: Blocking I/O, 阻塞I/O
@@ -18,10 +15,15 @@ public class BIOServer {
 
         try {
             ServerSocket serverSocket = new ServerSocket(9981);
-            // 线程池
+            /**
+             * 使用线程池实现伪异步IO
+             */
             // 不建议使用Executors创建，应使用ThreadPoolExecutor来创建
-             ExecutorService executorService = Executors.newFixedThreadPool(5);
+//             ExecutorService executorService = Executors.newFixedThreadPool(5);
 //            Executors.newSingleThreadExecutor()
+            ExecutorService executorService =
+                    new ThreadPoolExecutor(3, 10, 60L,
+                            TimeUnit.SECONDS, new ArrayBlockingQueue<>(3));
             while(true) {
                 System.out.println("等待连接...");
                 // 阻塞操作，放弃CPU
